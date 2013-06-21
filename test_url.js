9300fac6699ce3c482ff6a210f7faf6fb80d4b0c
Woonhyuk Baek <bwhyuk@gmail.com>
2013-06-21 23:38:29 +0900

    add fibonacci code & test case

--- test_url.js
+++ test_url.js
@@ -7,7 +7,12 @@
 var FeedParser = require('feedparser')
   , request = require('request');
 
-feed = 'http://feeds.feedburner.com/channy';
+feed = {
+	uri:'http://feeds.feedburner.com/channy',
+	headers: {
+		'If-Modified-Since': 'Thu, 16 Mar 2013 23:59:59 GMT'
+	}
+};
 
 request(feed)
   .pipe(new FeedParser())
@@ -19,4 +24,8 @@ request(feed)
   })
   .on('article', function(article){
     console.log('Got article: %s', article.title || article.description);
+	console.log('date : %s', article.date);
+	console.log('author : %s', article.author);
+	console.log('link : %s', article.link);
+	console.log('');
   });
